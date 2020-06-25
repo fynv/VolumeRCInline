394fc5565f9a7abac0812c1517d96d805904d271
feiy <feiy@nvidia.com>
2020-06-25 18:55:49 +0800

    Minor fix.

--- test_lobster.py
+++ test_lobster.py
@@ -5,11 +5,11 @@ import VolumeRCInline as vrc
 
 VK_FORMAT_R8G8B8A8_SRGB = 43
 
-raw_cthead = np.fromfile('lobster.raw', dtype=np.uint8)
-raw_cthead = np.reshape(raw_cthead, (56, 324, 301))
+raw_lobster = np.fromfile('lobster.raw', dtype=np.uint8)
+raw_lobster = np.reshape(raw_lobster, (56, 324, 301))
 
 ray_caster = vrc.SimpleMIP()
-gpuvol = vrc.U8Volume(ray_caster, raw_cthead, (1.0, 1.0, 1.4))
+gpuvol = vrc.U8Volume(ray_caster, raw_lobster, (1.0, 1.0, 1.4))
 ray_caster.set_camera((150.0, 200.0, 100.0), (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), 45.0)
 
 colorBuf = vki.Texture2D(640, 480, VK_FORMAT_R8G8B8A8_SRGB)
